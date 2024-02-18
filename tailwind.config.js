0150a57a08b948b8f218e45f73cf688c00f59de2
tphrmmung <153507902+tphrmmung@users.noreply.github.com>
2024-02-18 14:48:56 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['Noto Sans Thai', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes : ["light", "dark", "cupcake", "synthwave"],
+  },
+  
+}
